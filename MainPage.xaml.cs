95be0518249d501840f39da947b14564fe6e511d
GrapeGummies <jayda.hickey@dcmail.ca>
2025-12-10 01:02:19 -0500

    Created a working add task button.

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,12 +1,34 @@
-﻿namespace ToDoList
+﻿using System.Collections.ObjectModel;
+
+namespace ToDoList
 {
     public partial class MainPage : ContentPage
     {
+        private ObservableCollection<string> task = new ObservableCollection<string>();
 
         public MainPage()
         {
             InitializeComponent();
+            DateTime currentDate = DateTime.UtcNow;
+            collectionTask.ItemsSource = task;
         }
 
+        private void Clicked_AddTask(object sender, EventArgs e)
+        {
+
+            if (!string.IsNullOrEmpty(entryUserTask.Text))
+            {
+                //labelDate.Text = "hi";
+                task.Add(entryUserTask.Text);
+                //labelDate.Text = entryUserTask.Text;
+                entryUserTask.Text = string.Empty;
+            }
+        }
+
+        private void Clicked_RemoveTask(object sender, EventArgs e)
+        {
+
+            
+        }
     }
 }
